5b95ea05f72aa5e5e1e91dd5788d958e00f90f28
Luis Rodriguez-Lujan <luis.rodriguezl@upm.es>
2016-11-24 15:12:41 +0100

    Del docs

--- docs/scripts/neuroviewer/babylon-drawer/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-require("babylonjs");
-__export(require("./detail/BabylonDrawer"));
-__export(require("./detail/BabylonPalette"));
-__export(require("./detail/BabylonConfigs"));
-//# sourceMappingURL=index.js.map
--- docs/scripts/neuroviewer/control/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-// Export all control elements
-__export(require("./detail/NvControl"));
-//# sourceMappingURL=index.js.map
